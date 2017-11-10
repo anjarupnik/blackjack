5c4d620d32b1dce93f097200db9454e9f1bbb15f
robdir <rhwickham@gmail.com>
2017-11-10 16:41:48 +0100

    deploy

--- src/api/client.js
+++ src/api/client.js
@@ -11,7 +11,6 @@ export default class ApiClient {
     this.host = process.env.NODE_ENV === 'production'
     ? `https://molten-handbill.codaisseur.cloud`
     : (host || `http://localhost:3030`)
-  this.host = host || process.env.API_URL || `http://localhost:3030`
   this.options = { ...this.defaultOptions, ...options }
   }
   // Authenticate and store the token
